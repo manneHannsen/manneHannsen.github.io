2b72ebf692d92d372859025ab9fa872613ef3db6
manneHannsen <48886104+manneHannsen@users.noreply.github.com>
2020-04-26 21:56:51 +0200

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+$('a').click(function(e){
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('body, html').animate({
+        scrollTop: $(target).offset().top - 200
+    }, 250);
+});
+
+
+// $('.scroll').on('click', function(e) {
+// 	e.preventDefault();
+// 	var target = $(this).attr('href');
+// 	var offset = 0;
+// 	if ($(this).data('offset') != undefined) offset = $(this).data('offset');
+// 	$.scrollTo(target, 300, { offset: -offset });
+// });
